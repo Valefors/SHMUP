b5ab145b44b58de36007fab9c162e734ef0e35ae
Unknown <pifi77@hotmail.fr>
2018-12-15 20:35:38 +0100

    [Add] Movement for player
    Basic movement (just input)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Transform _transform;
+
+
+    private static string _VERTICAL_AXIS = "Vertical";
+    private static string _HORIZONTAL_AXIS = "Horizontal";
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lXmovValue = Input.GetAxis(_HORIZONTAL_AXIS);
+        float lYmovValue = Input.GetAxis(_VERTICAL_AXIS);
+
+        if (lXmovValue != 0 || lYmovValue != 0)
+        {
+            Vector3 lMovement = new Vector3(lXmovValue, lYmovValue,0);
+            lMovement = lMovement.normalized * _speed * Time.deltaTime;
+
+
+            _transform.Translate(lMovement);
+        }
+
+
+
+    }
+}
