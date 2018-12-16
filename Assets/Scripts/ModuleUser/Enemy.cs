25b9878a57e6119c2e187624962599e04bd4c83c
DINO <pifi77@hotmail.fr>
2018-12-16 17:45:09 +0100

    [Update] Return to normal entity model

--- Assets/Scripts/ModuleUser/Enemy.cs
+++ Assets/Scripts/ModuleUser/Enemy.cs
@@ -2,40 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : ModuleUser
+public class Enemy : MonoBehaviour
 {
+
+    [Header("Personal Datas")]
+    [SerializeField]
+    protected Transform _spawnShot;
+    protected Transform _transform;
+    [SerializeField]
+    protected GameObject _prefabShot;
+
+
     [Header("Gameplay Datas")]
-    private int pv = 1;//PV ? List module plutôt ?
+    [SerializeField]
+    protected float _speed;
+    private int _pv = 1;//PV ? List module plutôt ?
+    [SerializeField]
+    protected float _shotRate = 0.1f;
+
+    private bool _canShoot = true;
 
     private void Update()
     {
         ManageShoot();
     }
 
-    public override void GetHit()
+    #region Shoot_region
+    protected virtual void ManageShoot()
     {
-        pv--;
-        if(pv <= 0)
+        if (_canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    protected virtual void Shoot()
+    {
+        Shot shotShot = Instantiate(_prefabShot, _spawnShot.position, Quaternion.identity).GetComponent<Shot>();
+        shotShot.SetUp(false, Vector3.up);
+        _canShoot = false;
+        Invoke("CanShootAgain", _shotRate);
+    }
+
+    protected virtual void CanShootAgain()
+    {
+        _canShoot = true;
+    }
+
+    #endregion
+
+    #region GetDamage
+    public virtual void GetHit()
+    {
+        _pv--;
+        if (_pv <= 0)
             Death();
     }
+
+    #endregion
 
     private void Death()
     {
         Destroy(this.gameObject);
     }
 
-    #region Abstract
 
-    protected override bool Ally()
-    {
-        return false;
-    }
-
-    protected override Vector3 ShotDirection()
-    {
-        return Vector3.down;
-    }
-
-    #endregion
 
 }
