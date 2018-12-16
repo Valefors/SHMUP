1f50d29af7edf95e01a0b578ceec4c1ad081b9f3
DINO <pifi77@hotmail.fr>
2018-12-16 16:27:39 +0100

    gitflow-feature-stash: CorrectInherits

---
+++ Assets/Scripts/ModuleUser/ModuleUser.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ModuleUser : MonoBehaviour
+{
+
+    [Header("Personal Datas")]
+    [SerializeField]
+    protected Transform _spawnShot;
+    protected Transform _transform;
+    [SerializeField]
+    protected GameObject _prefabShot;
+
+
+    [Header("Gameplay Datas")]
+    [SerializeField]
+    protected float _speed;
+    [SerializeField]
+    protected float _shotRate = 0.1f;
+
+
+
+    private bool _canShoot = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AccessibleStart();
+    }
+
+    // Start is called before the first frame update
+    protected virtual void AccessibleStart()
+    {
+        _transform = this.transform;
+    }
+
+
+    #region Shoot_region
+    protected virtual void ManageShoot()
+    {
+        if (_canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    protected virtual void Shoot()
+    {
+        Shot shotShot = Instantiate(_prefabShot, _spawnShot.position, Quaternion.identity).GetComponent<Shot>();
+        shotShot.SetUp(!Ally(), ShotDirection());
+        _canShoot = false;
+        Invoke("CanShootAgain", _shotRate);
+    }
+
+    protected virtual void CanShootAgain()
+    {
+        _canShoot = true;
+    }
+
+    #endregion
+
+    protected abstract bool Ally();
+    protected abstract Vector3 ShotDirection();
+
+    #region GetDamage
+    public virtual void GetHit()
+    {
+        Debug.Log("I'm getting hit ! Outch !");
+        //pv--;
+        //Death();
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/ModuleUser/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : ModuleUser
+{
+    private static string _VERTICAL_AXIS = "Vertical";
+    private static string _HORIZONTAL_AXIS = "Horizontal";
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lXmovValue = Input.GetAxis(_HORIZONTAL_AXIS);
+        float lYmovValue = Input.GetAxis(_VERTICAL_AXIS);
+
+        if (lXmovValue != 0 || lYmovValue != 0)
+        {
+            Move(lXmovValue, lYmovValue);
+        }
+
+        if (Input.GetAxisRaw("Fire1") != 0)
+        {
+            ManageShoot();
+        }
+    }
+
+    void Move(float lXmovValue, float lYmovValue)
+    {
+        Vector3 lMovement = new Vector3(lXmovValue, lYmovValue, 0);
+        lMovement = lMovement.normalized * _speed * Time.deltaTime;
+
+        _transform.Translate(lMovement);
+    }
+
+
+
+
+    #region Abstract
+
+    protected override bool Ally()
+    {
+        return true;
+    }
+
+    protected override Vector3 ShotDirection()
+    {
+        return Vector3.up;
+    }
+
+    #endregion
+
+}
