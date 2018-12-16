2c4ca8a72cc393c42f3d4228c3d202441a7b19d8
DINO <pifi77@hotmail.fr>
2018-12-16 16:27:39 +0100

    gitflow-feature-stash: CorrectInherits

---
+++ Assets/Scripts/ModuleUser/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : ModuleUser
+{
+    [Header("Gameplay Datas")]
+    private int pv = 1;//PV ? List module plutôt ?
+
+    private void Update()
+    {
+        ManageShoot();
+    }
+
+    public override void GetHit()
+    {
+        pv--;
+        if(pv <= 0)
+            Death();
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+    #region Abstract
+
+    protected override bool Ally()
+    {
+        return false;
+    }
+
+    protected override Vector3 ShotDirection()
+    {
+        return Vector3.down;
+    }
+
+    #endregion
+
+}
