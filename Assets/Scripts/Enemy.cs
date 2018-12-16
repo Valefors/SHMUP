e45483755c36f7244d7823995b24ec5949d302d2
DINO <pifi77@hotmail.fr>
2018-12-16 14:03:17 +0100

    [UPDATE]  Enemy shot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    
+    [Header("Personal Datas")]
     private int pv = 1;
+    private float _shotRate = 0.2f;
+    [SerializeField]
+    private Transform _spawnShot;
+    private Transform _transform;
+    [SerializeField]
+    private GameObject _prefabShot;
+
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        ManageShoot();
+    }
+
+    void ManageShoot()
+    {
+        if (_canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Shot shotShot = Instantiate(_prefabShot, _spawnShot.position, Quaternion.identity).GetComponent<Shot>();
+        shotShot.SetUp(true, Vector3.down);
+        _canShoot = false;
+        Invoke("CanShootAgain", _shotRate);
+    }
+
+    void CanShootAgain()
+    {
+        _canShoot = true;
+    }
+
 
     public void GetHit()
     {
         pv--;
-        Death();
+        if(pv <= 0)
+            Death();
     }
 
     private void Death()
     {
         Destroy(this.gameObject);
     }
+
+
 
 }
--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
     [SerializeField]
+    private SpriteRenderer _spriteRdr;
+
+    [SerializeField]
     private float _speedShot = 1f;
+    private bool _isEnemy = true;
 
     [SerializeField]
     private Vector3 _direction = Vector3.up;
 
 
+
     private Transform _transform;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision with " + collision.gameObject.name);
+        //Debug.Log("Collision with " + collision.gameObject.name);
+
         Enemy enemyColl = collision.gameObject.GetComponent<Enemy>();
-        if (enemyColl != null)
+        if (enemyColl != null && !_isEnemy)
         {
             enemyColl.GetHit();
             this.Touch();
         }
+
+        Player playerColl = collision.gameObject.GetComponent<Player>();
+        if (playerColl != null && _isEnemy)
+        {
+            playerColl.GetHit();
+            this.Touch();
+        }
     }
-    
     
     // Start is called before the first frame update
     void Start()
@@ -36,6 +48,34 @@ public class Shot : MonoBehaviour
     {
         _transform.Translate(_direction * _speedShot * Time.deltaTime);
     }
+
+    //__TO DO : To change PROVISOIRE
+    public void SetUp(bool isEnemy, Vector3 direction)
+    {
+        SetUp(isEnemy, direction, _speedShot);
+    }
+
+    public void SetUp(bool isEnemy, Vector3 direction, float speed)
+    {
+        this._isEnemy = isEnemy;
+        this._direction = direction;
+        this._speedShot = speed;
+
+        ChangeColor();
+    }
+
+    public void ChangeColor()
+    {
+        if (_isEnemy)
+        {
+            _spriteRdr.color = Color.red;
+        }
+        else
+        {
+            _spriteRdr.color = Color.yellow;
+        }
+    }
+
 
     private void Touch()
     {
