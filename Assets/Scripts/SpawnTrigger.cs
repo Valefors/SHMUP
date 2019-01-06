5b8b4801ed55098d143af43395e5a100acb0dc8a
Valefor <emeline.berenguier@gmail.com>
2019-01-06 16:36:32 +0100

    [ADD] Spawn Enemies

---
+++ Assets/Scripts/SpawnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject[] _wavesArray;
+    bool _isActive = false;
+
+    private void OnTriggerEnter2D(Collider2D pCol)
+    {
+        if (pCol.gameObject.tag == "Player" && !_isActive)
+        {
+            LaunchWaves();
+            _isActive = true;
+        }
+    }
+
+    void LaunchWaves()
+    {
+        for (int i = 0; i < _wavesArray.Length; i++)
+        {
+            Instantiate(_wavesArray[i]);
+        }
+    }
+}
