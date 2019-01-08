b8289681d20c5476b2593131534fe4838d237279
Axel <axel.malherbe0@gmail.com>
2019-01-08 18:15:34 +0100

    [ADD] Music, Sorted folders, Tweek guns and ennemies

--- Assets/Scripts/ScrollingTestLD.cs
+++ Assets/Scripts/ScrollingTestLD.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scrolling : MonoBehaviour
+public class ScrollingTestLD : MonoBehaviour
 {
     public float scrollingSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
