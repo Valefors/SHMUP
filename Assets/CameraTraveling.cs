64266316ba497d9ab1cb4eeaba7e5523cfd3ce92
Axel <axel.malherbe0@gmail.com>
2019-01-25 01:17:49 +0100

    [UPDATED] Falling Buildings WIP

--- Assets/CameraTraveling.cs
+++ Assets/CameraTraveling.cs
@@ -7,20 +7,52 @@ public class CameraTraveling : MonoBehaviour
 
     public float speed;
     Rigidbody rb;
+    public bool isCollapseTrigger=false;
+    float spentTime = 0;
+    public float timeToWait;
+    public float timeToPush;
+    public bool isActor=false;
+    ParticleSystem[] temp;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if(isActor)
+        {
+            temp= GetComponentsInChildren<ParticleSystem>();
+            Debug.Log(temp.Length);
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i].Simulate(1);
+                temp[i].Play();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Vector3 newCoord = new Vector3(transform.position.x, transform.position.y, transform.position.z+speed);
-        transform.SetPositionAndRotation(newCoord, transform.rotation);
-        //transform.Translate(Vector3.right * speed);
-
-        //rb.MovePosition(newCoord);
+        if(isActor)
+        {
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i].Simulate(1);
+                temp[i].Play();
+            }
+        }
+        if (!isCollapseTrigger)
+        {
+            Vector3 newCoord = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+            transform.SetPositionAndRotation(newCoord, transform.rotation);
+        }
+        else
+        {
+            spentTime += Time.deltaTime;
+            if(spentTime>=timeToWait && spentTime<=timeToWait+timeToPush)
+            {
+                Vector3 newCoord = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+                transform.SetPositionAndRotation(newCoord, transform.rotation);
+            }
+        }
     }
 }
---
+++ Assets/SandCollide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCollide : MonoBehaviour
+{
+    float timeSpent=0;
+    public float timeToWait;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToWait += Time.deltaTime;
+        if(timeSpent>=timeToWait)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
